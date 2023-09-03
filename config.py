d1ef31789e40aa24d83c4fe853af7d2f8442322d
hui09813h <126428744+hui09813h@users.noreply.github.com>
2023-09-03 14:06:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.cizar3lczvxa.us-east-1.rds.amazonaws.com"
+customhost = "employee.cavs6dhxfekn.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "peyhuishan-employee"
 customregion = "us-east-1"
 
